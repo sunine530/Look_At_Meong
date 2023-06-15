375659494a748fc02f458eaa2916fa9f48b9c70a
sunine530 <133097223+sunine530@users.noreply.github.com>
2023-06-15 16:48:19 +0900

    기초 셋팅

---
+++ src/main/java/com/su/look_at_meong/LookAtMeongApplication.java
@@ -0,0 +1,13 @@
+package com.su.look_at_meong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookAtMeongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LookAtMeongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/su/look_at_meong/LookAtMeongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.su.look_at_meong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookAtMeongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
