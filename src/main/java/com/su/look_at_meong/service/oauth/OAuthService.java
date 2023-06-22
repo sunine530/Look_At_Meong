e84808e5831929082d9201f45a641ca1457a0edc
sunine530 <133097223+sunine530@users.noreply.github.com>
2023-06-22 12:18:12 +0900

    Feat : [005-KAKAO-LOGIN] 카카오 로그인 요청시 회원가입 유무 판단 등록

--- src/main/java/com/su/look_at_meong/service/oauth/OAuthService.java
+++ src/main/java/com/su/look_at_meong/service/oauth/OAuthService.java
@@ -1,6 +1,11 @@
 package com.su.look_at_meong.service.oauth;
 
+import com.su.look_at_meong.model.member.dto.TokenDto;
+import com.su.look_at_meong.model.member.entity.Member;
+import com.su.look_at_meong.repository.MemberRepository;
 import java.util.HashMap;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -15,6 +20,7 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RequiredArgsConstructor
 public class OAuthService {
 
     @Value("${oauth2.kakao.restApiKey}")
@@ -23,7 +29,9 @@ public class OAuthService {
     @Value("${oauth2.kakao.redirectUrl}")
     private String REDIRECT_URL;
 
-    RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate;
+
+    private final MemberRepository memberRepository;
 
     public String getKaKaoAccessToken(String code) {
 
@@ -39,6 +47,7 @@ public class OAuthService {
         params.add("code", code);
 
         HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+        System.out.println(kakaoTokenRequest);
 
         ResponseEntity<String> response = restTemplate.exchange(
             reqURL,
@@ -48,7 +57,6 @@ public class OAuthService {
         );
 
         String tokenJson = response.getBody();
-
         JSONParser jsonParser = new JSONParser();
         JSONObject jsonObject;
 
@@ -61,24 +69,26 @@ public class OAuthService {
         return jsonObject.get("access_token").toString();
     }
 
-    public HashMap<String, String> getUserInfoByKakao(String accessToken) {
+    public HashMap<String, String> getKakaoInfo(String accessToken) {
 
         String reqURL = "https://kapi.kakao.com/v2/user/me";
 
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", "Bearer " + accessToken);
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
-        HttpEntity<MultiValueMap<String, String>> kakapUserRequest = new HttpEntity<>(headers);
+        HttpEntity<MultiValueMap<String, String>> kakaoUserRequest = new HttpEntity<>(headers);
 
         ResponseEntity<String> response = restTemplate.exchange(
             reqURL,
             HttpMethod.POST,
-            kakapUserRequest,
+            kakaoUserRequest,
             String.class
         );
 
-        String userInfoJson = response.getBody();
+        System.out.println("카카오 서버에서 정상적으로 데이터를 수신했습니다.");
 
+        String userInfoJson = response.getBody();
         JSONParser jsonParser = new JSONParser();
         JSONObject jsonObject;
 
@@ -96,5 +106,24 @@ public class OAuthService {
         userInfo.put("email", kakao_account.get("email").toString());
 
         return userInfo;
+    }
+
+    public String getKakaoToken(Map<String, String> userInfo) {
+
+        String email = userInfo.get("email");
+        String nickname = userInfo.get("nickname");
+
+        if (!isExist(email)) {
+            memberRepository.save(Member.builder()
+                .name(nickname)
+                .email(email)
+                .build());
+        }
+
+        return null;
+    }
+
+    private boolean isExist(String email) {
+        return memberRepository.existsByEmail(email);
     }
 }
